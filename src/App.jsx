b013e402547229541dac05a82141d2daef6b029c
Denzel <lawrencedenzel60@gmail.com>
2024-07-11 11:43:13 +0300

    Setting up the environment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SignIn from './components/googleSignIn/SignIn'
+
+function App() {
+
+  return (
+    <>
+      <SignIn />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/googleSignIn/SignIn.jsx
@@ -0,0 +1,9 @@
+function SignIn() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default SignIn
